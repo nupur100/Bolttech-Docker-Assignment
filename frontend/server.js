661bf8f51226996818e19fc4ddaedac38b091aad
Nupur Goyal <37748784+nupur100@users.noreply.github.com>
2020-09-03 20:57:44 +0530

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,24 +1,27 @@
 const express = require('express');
+const request = require('request');
+const clients = require('./utils/clients');
+let ejs = require('ejs');
+
 const app = express();
 const port = 8084;
-const url = process.env.APP_URL
-//app.use(function(req, res, next) {
-//  res.header('Access-Control-Allow-Origin', '*');
-//  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//  next();
-//});
-app.use(express.static(__dirname + './views'));
-let ejs = require('ejs');
+
 app.set('views', './views')
 app.set('view engine', 'ejs');
-app.get('/', function(req, res){
 
-    res.render('index', {url: process.env.APP_URL})
+app.get('/', (req, res) => {
+      clients((error, clientsData) => {
+          if (error) {
+              return res.send({ error });
+          }
+          console.log(clientsData)
+          console.log(clientsData['clients'])
+          console.log({clients: clientsData['clients']})
+          res.render('index', clientsData['clients']);
 
-     });
+      });
+});
+//[ { clientName: { S: 'Alex' } } ] == clientsData['clients']
 
 app.listen(port, () => console.log(`Node app is listening at http://localhost:${port}`))
-
-
-
 
